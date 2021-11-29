06f20d4e47215651abb9ae370ae40c7701225e62
Annarath <annarathunun21@gmail.com>
2021-11-29 20:02:17 +0000

    add df

--- Resort Improvement Project/Data.py
+++ Resort Improvement Project/Data.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
+from pkg_resources import add_activation_listener
 import seaborn as sns
 import matplotlib.pyplot as plt
 import tabula as tb
 import re
+
+from tabula.io import convert_into
 import pdfplumber
 import camelot
 from tabula import read_pdf
@@ -12,3 +15,10 @@ import tabulate
 file = read_pdf("/Users/unun/Desktop/Tosang Beach Project/Day Data/31-10-21.PDF")
 file
 
+tb.convert_into("/Users/unun/Desktop/Tosang Beach Project/Day Data/31-10-21.PDF", "first_try_df.csv", output_format="csv")
+
+file['Adt']
+
+df = pd.read_csv("first_try_df.csv")
+
+df['Adt']
