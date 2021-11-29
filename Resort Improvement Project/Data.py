8cd9c6f7d953af67411af9bbf9293b5ddbdb1303
Annarath <annarathunun21@gmail.com>
2021-11-29 19:48:29 +0000

    Update Data.py

--- Resort Improvement Project/Data.py
+++ Resort Improvement Project/Data.py
@@ -4,6 +4,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import tabula as tb
 import re
+import pdfplumber
+import camelot
+from tabula import read_pdf
+import tabulate
 
-room = '01-10-21.pdf'
-data = tb.read_pdf(room)
+file = read_pdf("/Users/unun/Desktop/Tosang Beach Project/Day Data/31-10-21.PDF")
+file
+
