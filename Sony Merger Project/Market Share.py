243590711d45183f048ac9db015cf1d944d842c1
Annarath <annarathunun21@gmail.com>
2021-12-03 11:24:36 +0000

    Update Market Share.py

--- Sony Merger Project/Market Share.py
+++ Sony Merger Project/Market Share.py
@@ -3,4 +3,9 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-dfms = pd.read_excel(r'/Users/unun/Downloads/UK revenue 2015-2020.xlsx')
+dfms = pd.read_csv("/Users/unun/Desktop/Sony Project/UK revenue 2015-2020.csv")
+dfms
+
+#calculate HHI
+
+dfms['Sony'].plot(kind='hist', bins=30)
